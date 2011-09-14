50bb3aed79fb113e19f9c837641a2310240ada9c
Gershon Bialer <gershon.bialer@gmail.com>
2011-09-14 05:16:16 -0500

    Fixed config typo

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 // By default this uses environment variables. You can also put your Rayash userid and secret directly in this file if you wish. For example you could do:
 //     $RAYDASH_SECRET="MY SECRET";
 //     $RAYDASH_USERID="MY USERID";
-$RADYASH_USERID=getenv('RAYDASH_USERID');
+$RAYDASH_USERID=getenv('RAYDASH_USERID');
 $RAYDASH_SECRET=getenv('RAYDASH_SECRET');
 ?>
